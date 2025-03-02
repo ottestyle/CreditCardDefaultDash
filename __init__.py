4977cb6911e14f23d903de879c5dd91850ec4c95
ottestyle <alexander.otte19@gmail.com>
2025-03-02 10:39:18 +0100

    new changes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
+from .raw_callbacks import register_raw_callbacks
 from .clean_callbacks import register_clean_callbacks
 from .models_callbacks import register_models_callbacks
 
 def register_callbacks(app, default_data, eval_dict, y_test):
+    register_raw_callbacks(app, default_data)
     register_clean_callbacks(app, default_data)
     register_models_callbacks(app, eval_dict, y_test)
